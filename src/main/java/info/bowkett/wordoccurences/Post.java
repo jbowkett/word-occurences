58ae5e71126fb79c1a8bddc2d00e2096d6c947c2
jbowkett <james_bowkett@lyagushka.co.uk>
2014-10-14 07:25:46 +0000

    First check in of domain class.

---
+++ src/main/java/info/bowkett/wordoccurences/Post.java
@@ -0,0 +1,34 @@
+package info.bowkett.wordoccurences;
+
+import java.util.Date;
+
+/**
+ * Created by jbowkett on 14/10/2014.
+ */
+public class Post {
+
+  private final String title, link, comments, creator, guid, desciption, commentRssLink, slash, author;
+  private final String [] categories, symbols, stockSymbols, content;
+  private final Date publishingDate;
+
+  public Post(String title, String link, String comments, String creator,
+              String guid, String desciption, String commentRssLink,
+              String slash, String author, String[] categories,
+              String[] symbols, String[] stockSymbols, String[] content,
+              Date publishingDate) {
+    this.title = title;
+    this.link = link;
+    this.comments = comments;
+    this.creator = creator;
+    this.guid = guid;
+    this.desciption = desciption;
+    this.commentRssLink = commentRssLink;
+    this.slash = slash;
+    this.author = author;
+    this.categories = categories;
+    this.symbols = symbols;
+    this.stockSymbols = stockSymbols;
+    this.content = content;
+    this.publishingDate = publishingDate;
+  }
+}

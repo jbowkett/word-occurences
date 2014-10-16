e54bcbb6c5109395b58f4c5ce83598eb44fa0580
jbowkett <james_bowkett@lyagushka.co.uk>
2014-10-16 19:41:51 +0000

    Adds Blacklist loader + test

--- src/main/java/info/bowkett/wordoccurences/BlackListLoader.java
+++ src/main/java/info/bowkett/wordoccurences/BlackListLoader.java
@@ -1,6 +1,7 @@
 package info.bowkett.wordoccurences;
 
-import java.io.File;
+import java.io.*;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,13 +9,44 @@ import java.util.Set;
  * Created by jbowkett on 14/10/2014.
  */
 public class BlackListLoader {
-  private final File blackList;
+
+  private Set<String> blackList = new HashSet<String>();
 
   public BlackListLoader(File blackList) {
-    this.blackList = blackList;
+    load(blackList);
+  }
+
+  private void load(File file) {
+
+    FileReader fr = null;
+    BufferedReader br = null;
+    try{
+      fr = new FileReader(file);
+      br= new BufferedReader(fr);
+      String line = null;
+      while((line = br.readLine() )!= null){
+        final String[] words = line.split(",");
+        Collections.addAll(blackList, words);
+      }
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    finally{
+      try {
+        if (br != null) br.close();
+        if (fr != null) fr.close();
+      }
+      catch(IOException ioe){
+        //do nothing on close of resources
+      }
+    }
   }
 
   public Set<String> getBlackList() {
-    return new HashSet<String>();
+    return blackList;
   }
 }
